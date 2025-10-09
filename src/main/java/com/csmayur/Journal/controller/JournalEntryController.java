9c7a4f462823ed5af010d39c3d56a29626aaaa98
mayur0614 <cmayur.1404@gmail.com>
2025-10-09 21:46:44 +0530

    Initial commit

---
+++ src/main/java/com/csmayur/Journal/controller/JournalEntryController.java
@@ -0,0 +1,34 @@
+package com.csmayur.Journal.controller;
+
+import com.csmayur.Journal.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long,JournalEntry> journalEntries = new HashMap<>() ;
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public  boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true ;
+    }
+    @GetMapping("id/{myId}")
+    public JournalEntry findById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+        @DeleteMapping("id/{myId}")
+        public JournalEntry deleteEntryById(@PathVariable Long myId){
+            return journalEntries.remove(myId);
+        }
+}
