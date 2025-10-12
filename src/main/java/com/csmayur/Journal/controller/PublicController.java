b39237d256efe278f6102fa0e073b9315cc1f559
mayur0614 <cmayur.1404@gmail.com>
2025-10-12 19:17:16 +0530

    Initial commit

---
+++ src/main/java/com/csmayur/Journal/controller/PublicController.java
@@ -0,0 +1,39 @@
+package com.csmayur.Journal.controller;
+
+import com.csmayur.Journal.entity.UserEntity;
+import com.csmayur.Journal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("public")
+public class PublicController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder ;
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK kk";
+    }
+
+
+
+    @PostMapping("/create-user")
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity) {
+        // Encode password using injected bean
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+        userEntity.setRoles(Arrays.asList("USER"));
+        UserEntity savedUser = userService.saveEntry(userEntity);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+}
