fd02ac2455a072e4f8de2e021e140250ab05153a
mayur0614 <cmayur.1404@gmail.com>
2025-10-23 14:07:14 +0530

    fixed userservice for Render deployment

--- src/main/java/com/csmayur/Journal/service/UserService.java
+++ src/main/java/com/csmayur/Journal/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +23,7 @@ import java.util.Optional;
 @Slf4j
 @Component
 @Profile("dev")
+@Service
 public class UserService {
     @Autowired
     private UserEntryRepo userEntryRepo;
